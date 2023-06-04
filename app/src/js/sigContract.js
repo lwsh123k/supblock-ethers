fcad5e0a20f561f967866b9057c7e7659d04d3d0
lsj <lwsh123k@163.com>
2023-06-04 16:35:45 +0800

    add automine in random time

--- app/src/js/sigContract.js
+++ app/src/js/sigContract.js
@@ -14,7 +14,7 @@ const sigContract = {
     wallet: null,
     singerContract: null,
     contract: null,
-    contractAddress: '0x2279b7a0a67db372996a5fab50d91eaa73d2ebe6',
+    contractAddress: '0x5fbdb2315678afecb367f032d93f642f64180aa3',
 
     abi: [
         'function setResponseSig(address sender, bytes32 s, bytes32 t, bytes32 pkx, bytes32 pky) public',
--- hardhat.config.js
+++ hardhat.config.js
@@ -2,9 +2,16 @@ require('@nomicfoundation/hardhat-toolbox');
 require('@nomicfoundation/hardhat-chai-matchers'); // 引入断言
 
 /** @type import('hardhat/config').HardhatUserConfig */
+// 随机3-6s 产生一个区块
 module.exports = {
     solidity: '0.8.12',
     networks: {
         ganache: { url: 'http://127.0.0.1:7545' },
+        hardhat: {
+            mining: {
+                auto: false,
+                interval: [3000, 6000],
+            },
+        },
     },
 };
