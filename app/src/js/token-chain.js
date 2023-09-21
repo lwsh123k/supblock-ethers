abc330386d763c885c1025ed3e0683a1fc01d5ae
kskkk <hahahaha@163.com>
2023-09-21 22:14:46 +0800

    add script data

--- app/src/js/token-chain.js
+++ app/src/js/token-chain.js
@@ -12,6 +12,8 @@ const tokenChain = {
     b: [],
     hashForward: [],
     hashBackward: [],
+    chainLength: 3,
+    relayIndex: 0, // 向第几个relay发送数据
 
     // 对任意个数的参数取hash
     keccak256(...args) {
@@ -35,8 +37,8 @@ const tokenChain = {
     },
 
     // 链初始化用到的数据
-    computeInitialData(chainLength) {
-        for (let i = 0; i <= chainLength + 2; i++) {
+    computeInitialData() {
+        for (let i = 0; i <= this.chainLength + 2; i++) {
             // selectedTempAccount: 100个账户中只会用到一部分
             let randomIndex = Math.floor(Math.random() * 100);
             this.selectedTempAccount.push(this.tempAccount[randomIndex]);
@@ -47,19 +49,25 @@ const tokenChain = {
             // r: hash时混淆
             this.r.push(this.generateRandomByte(32));
         }
+        // selectedTempAccount中第一个账户为real name account
+        this.selectedTempAccount.pop();
+        this.selectedTempAccount.unshift(this.realNameAccount);
 
         // 计算hash
         this.hashForward.push(this.keccak256(this.selectedTempAccount[0], this.r[0]));
-        for (let i = 1; i <= chainLength + 1; i++) {
+        for (let i = 1; i <= this.chainLength + 1; i++) {
             this.hashForward.push(
                 this.keccak256(this.selectedTempAccount[i], this.r[i], this.hashForward[i - 1])
             );
         }
         this.hashBackward.unshift(
-            this.keccak256(this.selectedTempAccount[chainLength + 2], this.r[chainLength + 2])
+            this.keccak256(
+                this.selectedTempAccount[this.chainLength + 2],
+                this.r[this.chainLength + 2]
+            )
         );
         // 计算反向hash时, 每次都将数据放到数组头部
-        for (let i = chainLength; i >= 0; i--) {
+        for (let i = this.chainLength; i >= 0; i--) {
             this.hashBackward.unshift(
                 this.keccak256(this.selectedTempAccount[i + 1], this.r[i + 1], this.hashBackward[0])
             );
@@ -72,6 +80,53 @@ const tokenChain = {
         };
     },
 
+    // applicant向relay发送数据
+    getApp2RelayData() {
+        relayIndex = this.relayIndex;
+        if (relayIndex === 0)
+            return {
+                r: this.r[0],
+                hf: this.hashForward[0],
+                hb: this.hashBackward[0],
+                b: this.b[0],
+            };
+        else if (relayIndex >= 1 && relayIndex <= this.chainLength - 1)
+            return {
+                account: this.selectedTempAccount[relayIndex],
+                r: this.r[relayIndex],
+                hf: this.hashForward[relayIndex],
+                hb: this.hashBackward[relayIndex],
+                b: this.b[relayIndex],
+                c: 100,
+            };
+        else if (relayIndex === this.chainLength)
+            return {
+                account: this.selectedTempAccount[relayIndex],
+                r: this.r[relayIndex],
+                hf: this.hashForward[relayIndex],
+                hb: this.hashBackward[relayIndex],
+                c: 100,
+            };
+        else if (relayIndex === this.chainLength + 1)
+            return {
+                account: this.selectedTempAccount[relayIndex],
+                r: this.r[relayIndex],
+                hf: this.hashForward[relayIndex],
+                hb: this.hashBackward[relayIndex],
+            };
+        else if (relayIndex === this.chainLength + 2)
+            return {
+                account: this.selectedTempAccount[relayIndex],
+                r: this.r[relayIndex],
+            };
+    },
+
+    // relay向applicant发送数据
+    sendRelay2AppData() {},
+    //  前一个relay向后一个relay发送数据
+    sendToNextRelay() {},
+    // 后一个relay向前一个relay发送响应数据
+    SendToPreRelay() {},
     // 验证数据
     validateData(r0, hashForward) {
         return hashForward === this.keccak256(this.validatorAccount, r0);
